2026a8e01ec79e75466699e3d85491b34a13b57c
Lida <y_uguest@yahoo.co.uk>
2020-03-04 22:54:47 +0700

    billing by stripe

---
+++ billing.js
@@ -0,0 +1,25 @@
+import stripePackage from "stripe";
+import { calculateCost } from "./libs/billing-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
+
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
+
+  try {
+    await stripe.paymentIntents.create({
+      source,
+      amount,
+      description,
+      payment_method_types: ['card'],
+      currency: "usd"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
+}
