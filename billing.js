137bb64fed0ebe4051c89f6128d8c37c0dfe96b5
Lida <y_uguest@yahoo.co.uk>
2020-03-07 12:59:09 +0700

    add billing

--- billing.js
+++ billing.js
@@ -1,25 +1,25 @@
-// import stripePackage from "stripe";
-// import { calculateCost } from "./libs/billing-lib";
-// import { success, failure } from "./libs/response-lib";
+import stripePackage from "stripe";
+import { calculateCost } from "./libs/billing-lib";
+import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  // const { storage, source } = JSON.parse(event.body);
-  // const amount = calculateCost(storage);
-  // const description = "Scratch charge";
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
 
-  // // Load our secret key from the  environment variables
-  // const stripe = stripePackage(process.env.stripeSecretKey);
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
 
-  // try {
-  //   await stripe.paymentIntents.create({
-  //     source,
-  //     amount,
-  //     description,
-  //     payment_method_types: ['card'],
-  //     currency: "usd"
-  //   });
-  //   return success({ status: true });
-  // } catch (e) {
-  //   return failure({ message: e.message });
-  // }
+  try {
+    await stripe.paymentIntents.create({
+      source,
+      amount,
+      description,
+      payment_method_types: ['card'],
+      currency: "usd"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
 }
